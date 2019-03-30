42d1c13b343f5ca7677e6497c734306dca04ea7d
Beril Zhang <moujunki@gmail.com>
2019-03-29 17:17:44 -0700

    completed lesson 0

--- Lesson0/Lesson0/Source.cpp
+++ Lesson0/Lesson0/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
+#include"res_path.h"
 
 int main(int, char**)
 {
@@ -8,6 +10,8 @@ int main(int, char**)
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+
+	std::cout << "Resource path is: " << getResourcePath() << std::endl;
 	SDL_Quit();
 	return 0;
 }
---
+++ include/res_path.h
@@ -0,0 +1,35 @@
+#ifndef RES_PATH_H
+#define RES_PATH_H
+
+#include <iostream>
+#include <string>
+#include <SDL.h>
+
+std::string getResourcePath(const std::string& subDir = "") {
+
+#ifdef _WIN32
+	const char PATH_SEP = '\\';
+#else
+	const char PATH_SEP = '/';
+#endif
+
+	static std::string baseRes; //space for the static variable is allocated only once and the value of variable in the previous call gets carried through the next function call
+	if (baseRes.empty()) {
+		char* basePath = SDL_GetBasePath();
+		if (basePath) {
+			baseRes = basePath;
+			SDL_free(basePath); // dellocates
+		}
+		else {
+			std::cerr << "Error getting resource path: " << SDL_GetError() << std::endl;
+			return ""; 
+		}
+		size_t pos = baseRes.rfind("Debug");
+		baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
+
+
+	}
+	return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
+}
+
+#endif
